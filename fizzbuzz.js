8dbda5cd36dac867852cc0425547ce1b35c78351
goto florian <gotoflorian.pro@gmail.com>
2018-02-24 03:48:56 +0100

    add fizzbuzz algo

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+const fizzbuzz = num =>  {
+	for(var i = 0; i <= num; i++){
+		// place fizzbuzz conditional on top because only one condition executed
+		// divisible by 3 and 5, same as divisible by 15
+		if(i % 15 === 0) console.log('FizzBuzz');
+		else if (i % 3 === 0) console.log('Fizz');
+		else if (i % 5 === 0) console.log('Buzz');
+                else console.log(i);
+	}
+}
+
+fizzbuzz(40);
