91bb210d8c846bcc3c5a18c827c5393560777c98
goto florian <gotoflorian.pro@gmail.com>
2018-02-26 00:30:23 +0100

    add Is Palindrome algo

---
+++ isPalindrome.js
@@ -0,0 +1,27 @@
+/*
+    IS PALINDROME ALGORITHM
+
+    Take string input and check if is palindrome
+    - ignoring punctuation and case
+    - without regex
+*/
+
+const isPalindrome = stringInput => {
+    // spread operator to create array of characters
+    const iCharArr = [ ...( stringInput.toLowerCase().trim() ) ];
+    const validCharacters = [...'azertyuiopmlkjhgfdsqwxcvbn'];
+
+    // create an array containing only valid characters
+    let validICharArr = [];
+    iCharArr.forEach( char => {
+        if(validCharacters.includes(char))  
+            validICharArr.push(char);
+    });
+
+    const original = validICharArr.join('')
+    const reverse = validICharArr.reverse().join('')
+    
+    return console.log(`"${stringInput}" is ${original === reverse  ? '' : 'not '}a palindrome`);
+};
+
+isPalindrome(process.argv[2] || "Madam, I'm Adam");
